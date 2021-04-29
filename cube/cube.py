be14f52bd08f4d1daa08561ed7aeb5bfebcef0a9
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2021-04-29 20:27:40 +0500

    Created cube rotation field, created ray tracing signature

--- cube/cube.py
+++ cube/cube.py
@@ -1,15 +1,18 @@
 from typing import List, Tuple
+
 from utils import rotate_z, rotate_y, rotate_x
 
 
 class Cube:
     center: Tuple[float, float, float]
     nodes: List[Tuple[float, float, float]]
+    rotation: List[int, int, int]
     planes: List[List[int]]
 
     def __init__(self) -> None:
         self.center = (0.0, 0.0, 0.0)
         self.calculate_nodes()
+        self.rotation = [0, 0, 0]
 
     def calculate_nodes(self) -> None:
         self.nodes = [
@@ -33,6 +36,9 @@ class Cube:
         ]
 
     def rotate(self, angle_x: int, angle_y: int, angle_z: int) -> None:
+        self.rotation[0] += angle_x
+        self.rotation[1] += angle_y
+        self.rotation[2] += angle_z
         for node in range(len(self.nodes)):
             self.nodes[node] = rotate_z(self.nodes[node], angle_z)
             self.nodes[node] = rotate_x(self.nodes[node], angle_x)
---
+++ ray_tracing/ray_tracing.py
@@ -0,0 +1,6 @@
+from typing import Tuple
+
+
+def trace(screen_point: Tuple[int, int]) -> int:
+    """Return number of edge which is cross by ray"""
+    pass
